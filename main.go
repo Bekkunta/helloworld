18b1404354ebd56e3b3b4470f06a850544758088
Bekkunta <Xiaobek@gmail.com>
2020-11-26 15:09:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hellos i am branch man")
 	fmt.Println("hello to the new world from bekkunta + jiahao huehue <3")
 	fmt.Println("Ai yo sian leh")
+	fmt.Println("Hehhhh")
 }
